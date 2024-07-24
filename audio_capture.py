3efda422d434c0039c35483df54663edb1e86e16
Benji Gifford <90120192+benjigifford@users.noreply.github.com>
2024-07-23 22:44:34 -0400

    Create audio_capture.py

---
+++ audio_capture.py
@@ -0,0 +1,40 @@
+import pyaudio
+import wave
+
+def record_audio(file_name, record_seconds=60):
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 1
+    fs = 44100  # Record at 44100 samples per second
+
+    p = pyaudio.PyAudio()
+
+    print('Recording')
+
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+
+    frames = []
+
+    for _ in range(0, int(fs / chunk * record_seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    print('Finished recording')
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(file_name, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+if __name__ == '__main__':
+    record_audio('conversation.wav', 60)  # Record for 60 seconds
